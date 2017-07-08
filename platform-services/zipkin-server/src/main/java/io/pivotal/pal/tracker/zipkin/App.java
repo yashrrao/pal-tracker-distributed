e166af69cbfc8508a6b194a196032e615e11bab7
Tyson Gern <tgern@pivotal.io>
2017-07-08 14:27:45 -0600

    Enable tracing with zipkin

---
+++ platform-services/zipkin-server/src/main/java/io/pivotal/pal/tracker/zipkin/App.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+import java.util.TimeZone;
+
+@EnableZipkinStreamServer
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(App.class, args);
+    }
+}
