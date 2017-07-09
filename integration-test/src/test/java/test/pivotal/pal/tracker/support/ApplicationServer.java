38c3d956fee6c7265e04b33b3854c53d7fe9f348
Tyson Gern <tgern@pivotal.io>
2017-07-09 09:41:13 -0600

    Enable Config Server

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -42,6 +42,7 @@ public class ApplicationServer {
             .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
             .put("APPLICATION_OAUTH_ENABLED", "false")
             .put("SPRING_ZIPKIN_ENABLED", "false")
+            .put("REGISTRATION_SERVER_ENDPOINT", "http://registration-server")
             .build()
         );
     }
